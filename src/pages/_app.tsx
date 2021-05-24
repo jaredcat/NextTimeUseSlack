938cc539e7cd3c257e228d1f7845349e9e089c86
jaredkotoff <jaredkotoff@users.noreply.github.com>
2021-05-23 23:22:09 -0700

    chore: fix tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,17 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { globalStyles } from "../shared/styles";
 
+const title = "Next Time, Use Slack";
+const description =
+  "Ever wonder how much a meeting 'costs'? Easily calculate the cost of a meeting or start a timer to watch how much money you're burning. Next time, let's just use slack instead.";
+const siteUrl = "https://nexttimeuseslack.com";
+const previewImage = "https://nexttimeuseslack.com/images/burn.jpg";
+const twitterHandle = "@xz3r0";
+const fbAdminId = "578412433";
+
 const App = ({ Component, pageProps }: AppProps): React.ReactElement => (
   <>
     <Head>
-      <title>Next Time, Use Slack</title>
-      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta
-        name="description"
-        content="Ever wonder how much a meeting 'costs'? Easily calculate the cost of a meeting or start a timer to watch how much money you're burning. Next time, let's just use slack instead."
-      />
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
@@ -24,27 +25,43 @@ const App = ({ Component, pageProps }: AppProps): React.ReactElement => (
         href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;600&display=swap"
       />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@publisher_handle" />
-      <meta name="twitter:title" content="Page Title" />
+      <title>{title}</title>
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={description} />
+
+      {/* Facebook */}
+      <meta property="og:url" content={siteUrl} key="ogurl" />
+      <meta property="og:image" content={previewImage} key="ogimage" />
+      <meta property="og:site_name" content={title} key="ogsitename" />
+      <meta property="og:title" content={title} key="ogtitle" />
+      <meta property="og:description" content={description} key="ogdesc" />
+      <meta property="og:type" content="website" key="ogtype" />
+      <meta property="fb:admins" content={fbAdminId} key="fbadmins" />
+
+      {/* Twitter */}
+      <meta
+        name="twitter:card"
+        content="summary_large_image"
+        key="twittercard"
+      />
+      <meta name="twitter:site" content={twitterHandle} key="twittersite" />
+      <meta name="twitter:title" content={title} key="twittertitle" />
       <meta
         name="twitter:description"
-        content="Page description less than 200 characters"
+        content={description}
+        key="twitterdescription"
       />
-      <meta name="twitter:creator" content="@author_handle" />
-      {/* <!-- Twitter summary card with large image must be at least 280x150px --> */}
       <meta
-        name="twitter:image:src"
-        content="images/burn.jpg"
+        name="twitter:creator"
+        content={twitterHandle}
+        key="twittercreator"
       />
-
-      <meta property="og:title" content="Next Time, Use Slack" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://nexttimeuseslack.com/" />
-      <meta property="og:image" content="images/burn.jpg" />
-      <meta property="og:description" content="Description Here" />
-      <meta property="og:site_name" content="Next Time, Use Slack" />
-      <meta property="fb:admins" content="578412433" />
+      <meta
+        name="twitter:image"
+        content={previewImage}
+        key="twitterimage"
+      />
     </Head>
     {globalStyles}
     <Component {...pageProps} />
