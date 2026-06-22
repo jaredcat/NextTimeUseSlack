061311b0b2d9dd38752fd709dbaeb0b5be6cbf04
Jared.cat <3268576+jaredcat@users.noreply.github.com>
2026-06-22 13:37:05 -0700

    Fix GH page deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.GITHUB_PAGES === "true" ? "/NextTimeUseSlack" : "";
+
 const nextConfig = {
   output: "export",
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : undefined,
 };
 
 module.exports = nextConfig;
